29c97c7942073b1fbe96dc27a0faaf3e6376607b
Kai Xu <xukai921110@gmail.com>
2016-12-10 23:20:20 +0000

    Make it run

--- deep_dream.py
+++ deep_dream.py
@@ -3,19 +3,19 @@ from functools import partial
 import PIL.Image
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import urllib.request
+import urllib2
 import os
 import zipfile
 
 def main():
     #Step 1 - download google's pre-trained neural network
     url = 'https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip'
-    data_dir = '../data/'
+    data_dir = './data/'
     model_name = os.path.split(url)[-1]
     local_zip_file = os.path.join(data_dir, model_name)
     if not os.path.exists(local_zip_file):
         # Download
-        model_url = urllib.request.urlopen(url)
+        model_url = urllib2.urlopen(url)
         with open(local_zip_file, 'wb') as output:
             output.write(model_url.read())
         # Extract
